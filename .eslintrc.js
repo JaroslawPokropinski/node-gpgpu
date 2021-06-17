a4e5958329e350b8612f5fc6117ef612a5208dbf
Jarosław Pokropiński <jarek.pokropinski@gmail.com>
2021-06-17 12:36:57 +0200

    :art: Configure eslint for automatic fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-types': 'warn',
   },
 };
